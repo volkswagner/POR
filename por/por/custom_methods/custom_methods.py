bd8f36e7baca3a07ee340e0f336a1c5881da7299
Mark Gutierrez <mgutierrez@itgetit.com>
2025-09-14 15:49:06 -0400

    Clean up custom_methods calculations

--- por/por/custom_methods/custom_methods.py
+++ por/por/custom_methods/custom_methods.py
@@ -22,6 +22,7 @@ def calculate_rate_and_uom(doc, method):
 
             # set the rates
             overage_rate = frappe.get_value("Item Price", {"item_code": item.item_code, "uom": "Daily"}, "price_list_rate") // settings.daily_divisor
+            four_hour_rate = frappe.get_value("Item Price", {"item_code": item.item_code, "uom": "4hr"}, "price_list_rate")
             daily_rate = frappe.get_value("Item Price", {"item_code": item.item_code, "uom": "Daily"}, "price_list_rate")
             weekly_rate = frappe.get_value("Item Price", {"item_code": item.item_code, "uom": "Weekly"}, "price_list_rate")
             four_week_rate = frappe.get_value("Item Price", {"item_code": item.item_code, "uom": "4Week"}, "price_list_rate")
@@ -35,31 +36,36 @@ def calculate_rate_and_uom(doc, method):
             four_hour_chunks = (rounded_hours + settings.overage_billing_interval - 1) // settings.overage_billing_interval
             extra_charge = four_hour_chunks * overage_rate
 
-            # calculate for half day rate when returned on time
-            if duration <= (4 + settings.four_hr_grace):
-                rate = frappe.get_value("Item Price", {"item_code": item.item_code, "uom": "4hr"}, "price_list_rate")
-                uom = '4hr'
-                rental_units = '4hr 1'              
 
-            # calculate between 4-24 hours    
+            # check if date/time returned is earlier than date out!
+            if duration <= 0:
+                frappe.throw('Please verify date and time returned is after date and time out.')
+
+            # calculate for half day rate when returned on time
+            elif duration <= (4 + settings.four_hr_grace):
+                rate = four_hour_rate
+                uom = '4hr'
+                rental_units = '4hr:1'
+
+            # calculate between 4-24 hours
             elif duration <= (24 + settings.daily_grace):
                 rate = daily_rate
                 uom = 'Daily'
-                rental_units = 'D 1'
+                rental_units = 'D:1'
 
-            # calculate up to three days    
+            # calculate up to three days
             elif duration <= (72 + settings.daily_grace):
                 rate = daily_amount + extra_charge
                 uom = 'Daily'
-                rental_units = f"D {full_days}, OT {rounded_hours}"
+                rental_units = f"D:{full_days}, OT:{rounded_hours}"
 
             # calculate 3 days to one week
             elif duration <= (168 + settings.daily_grace):
                 rate = weekly_rate
                 uom = 'Weekly'
-                rental_units = 'Wk 1'
+                rental_units = 'Wk:1'
 
-            # calculate more than one week, less than two weeks    
+            # calculate more than one week, less than two weeks
             elif duration <= 236:
                 hours_remaining = duration - 168
                 full_days = int(hours_remaining / 24)
@@ -68,14 +74,14 @@ def calculate_rate_and_uom(doc, method):
                 rate = weekly_rate + (daily_rate * full_days) + (extra_charge if extra_hours > 0 else 0)
                 uom = 'Weekly'
                 #rental_units = f"Wk ew 1, wkRate {weekly_rate}, D {full_days}, dAmt {daily_amount} OT {rounded_hours} extra {extra_charge}"
-                rental_units = f"Wk 1, D {full_days}, OT {rounded_hours}"
+                rental_units = f"Wk:1, D:{full_days}, OT:{rounded_hours}"
 
             #calculate 4Week rate when greater than two weeks and <= 28 days
             elif duration <=  (672 + settings.daily_grace):
-                rate = four_week_rate 
+                rate = four_week_rate
                 uom = '4Week'
-                rental_units = "4Wk 1"  
-            
+                rental_units = "4Wk:1"
+
             # calculate all cases greater than 28 days
             else:
                 four_week_chunks = int(duration / 672)
@@ -87,28 +93,28 @@ def calculate_rate_and_uom(doc, method):
                     extra_hours = remainder_hours - (full_days * 24) - settings.daily_grace
                     rate = (four_week_rate * four_week_chunks) + (full_days * daily_rate) + extra_charge
                     uom = '4Week'
-                    rental_units = f"4Wk {four_week_chunks}, D {full_days}, OT {rounded_hours}"
+                    rental_units = f"4Wk:{four_week_chunks}, D:{full_days}, OT:{rounded_hours}"
 
                 # case when rounding to five weeks
                 elif remainder_hours <=  (168 + settings.daily_grace):
                     rate =  (four_week_rate * four_week_chunks) + weekly_rate
                     uom = '4Week'
-                    rental_units = "4Wk 1, Wk 1"
+                    rental_units = "4Wk:1, Wk:1"
 
-                # calculate monthly plus weekly rate and up to three days remaining before adding additional 4week    
+                # calculate monthly plus weekly rate and up to three days remaining before adding additional 4week
                 elif remainder_hours <= 236:
                     hours_remaining = remainder_hours - 168
                     full_days = int(hours_remaining / 24)
                     extra_hours = hours_remaining - (full_days * 24) - settings.daily_grace
                     rate = (four_week_chunks * four_week_rate) + (full_days * daily_rate) + weekly_rate + (extra_charge if extra_hours > 0 else 0)
                     uom = '4Week'
-                    rental_units = f"4Wk {four_week_chunks}, Wk 1, D {full_days}, OT {rounded_hours}"
+                    rental_units = f"4Wk:{four_week_chunks}, Wk:1, D:{full_days}, OT:{rounded_hours}"
 
 
                 elif remainder_hours > 236:
-                    rate = ((four_week_chunks +1) * four_week_rate) 
-                    uom = '4Week' 
-                    rental_units = f"4Wk {four_week_chunks +1}"  
+                    rate = ((four_week_chunks +1) * four_week_rate)
+                    uom = '4Week'
+                    rental_units = f"4Wk:{four_week_chunks +1}"
 
 
             item.rate = rate  # Set rate explicitly calculated every time
@@ -116,3 +122,15 @@ def calculate_rate_and_uom(doc, method):
             item.rental_uom = rental_units
 
     doc.calculate_taxes_and_totals()
+
+@frappe.whitelist()
+def get_items():
+    items = frappe.db.get_list('Item',
+        fields=['name', 'item_code', 'image'],
+    )
+    itemsWithCount = items
+
+    for item in itemsWithCount:  # traverse through items
+        item['rent_count'] = frappe.db.count('Sales Invoice Item', {'item_code' : item.item_code}) # add a value to store rent count
+
+    return itemsWithCount
